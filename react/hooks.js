de3b95957f50fc649f141ba2561c5fb8dddc7a50
eagr <eagr@tutanota.com>
2022-06-03 23:36:11 +0800

    react: useReducer()

---
+++ react/hooks.js
@@ -0,0 +1,39 @@
+/**
+ * Rules of hooks:
+ * hooks must be called from React functions
+ * hooks must be called from top level
+ */
+
+// TODO make it a React Component
+let component = {
+    hooks: [],
+}
+
+let hookIndex = 0
+function getHook (index) {
+    const hooks = component.hooks || (component.hooks = [])
+    while (index >= hooks.length) hooks.push({})
+    return hooks[index]
+}
+
+function useReducer (reducer, initState, initialize) {
+    const hook = getHook(hookIndex++)
+    if (!hook.reducer) hook.reducer = reducer
+
+    // having `comp` indicates initialization done
+    if (!hook.comp) {
+        hook.comp = component
+        const state = typeof initialize === 'function'
+            ? initialize(initState)
+            : typeof initState === 'function' ? initState() : initState
+        const dispatch = (action) => {
+            const next = hook.reducer(hook.state[0], action)
+            if (next !== hook.state[0]) {
+                hook.state = [next, hook.state[1]]
+                hook.comp.setState({})
+            }
+        }
+        hook.state = [state, dispatch]
+    }
+    return hook.state
+}
